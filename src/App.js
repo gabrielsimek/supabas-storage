6e64c4d57a9874da0fddb14d72c1f2ab4fbb50d9
Gabriel Simek <gabrielsimek@outlook.com>
2022-01-13 14:54:41 -0800

    get image url

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import { upload } from './services/images';
+import { upload, getImageUrl } from './services/images';
 import { useState } from 'react';
 import './App.css';
 
 function App() {
   const [file, setFile] = useState(null);
-
+  const [fileName, setFileName] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
   const handleUpload = async () => {
     const res = await upload(file);
     console.log(`\x1b[35m%s\x1b[0m`, 'res: ', res);
   };
 
+  const handleGetUrl = () => {
+    const url = getImageUrl(fileName);
+    setImageUrl(url);
+  };
+
   return (
+    // SET IMAGE
     <div className="App">
       <input
         type="file"
         onChange={(e) => {
-          console.log(`\x1b[35m%s\x1b[0m`, 'e.target.files: ', e.target.files, [...e.target.files]);
+          // console.log(`\x1b[35m%s\x1b[0m`, 'e.target.files: ', e.target.files, [...e.target.files]);
           setFile(e.target.files[0]);
         }}
       />
@@ -25,8 +32,25 @@ function App() {
             <img alt="output" src={URL.createObjectURL(file)} />
           </div>
           <button onClick={handleUpload}>Upload to Supabase</button>
+     
         </>
       )}
+  
+      {/* GET IMAGE */}
+      <div>
+        <h3>Image from Supabase</h3>
+        {imageUrl && <div className="output">
+          <img alt="output" src={imageUrl} />
+        </div>}
+        <input
+          type='text'
+          value={fileName} 
+          onChange={({ target }) => setFileName(target.value)}
+        />
+        <button onClick={handleGetUrl}>Get Image</button>
+       
+      </div>
+  
     </div>
   );
 }
--- src/services/images.js
+++ src/services/images.js
@@ -4,3 +4,15 @@ export const upload = async (file) => {
   const res = await client.storage.from('images').upload(`public/${file.name}`, file);
   return checkError(res);
 };
+
+
+export const getImageUrl = (fileName) => {
+  console.log(fileName);
+  const { publicURL, error } = client.storage
+    .from('images')
+    .getPublicUrl(`public/${fileName}`);
+
+  console.log(publicURL);
+  if (error) throw error;
+  else return publicURL;
+};
